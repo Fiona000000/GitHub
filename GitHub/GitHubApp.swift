ec1a98e1cf956c4264be0b678206893a1578f9de
Fiona <kyungwed@hotmail.com>
2023-08-14 10:19:33 -0400

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Fiona Beauchamp on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
